7544a15f1f23d6b90f9cf716725e37c76d8ed39b
Per Fahlén <perfahlen@gmail.com>
2015-10-20 23:42:11 +0200

    cleaned up methods

--- ItemMap/js/config.js
+++ ItemMap/js/config.js
@@ -28,26 +28,10 @@ smilOnline.config = function () {
         return mapOptions;
     };
 
-    var getFormConfiguration = function () {
-        var latitude = parseFloat(jQuery("#configWidgetFormLatitude").val());
-        var longitude = parseFloat(jQuery("#configWidgetFormLongitude").val());
-        var bingKey = jQuery("#configWidgetFormBingKey").val();
-        var zoomLevel = parseInt(jQuery("#configWidgetFormZoomLevel").val());
-        if (bingKey.length > 0) {
-            var location = { lat: latitude || 0, lon: longitude || 0 };
-            this.setCenter(location);
-            this.setZoom(zoomLevel);
-            this.setBingKey(bingKey);
-            var currentConfig = this.getConfig();
-            return currentConfig;
-        }
-    };
-
     return {
         setCenter: center,
         setZoom: zoom,
         setBingKey: bingKey,
-        getConfig: getConfig,
-        getFormConfiguration: getFormConfiguration
+        getConfig: getConfig
     };
 };
